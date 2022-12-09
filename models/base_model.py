28915d3734e4db517b4c6d8d1a62be5267b64677
gims-inc <gabrielmusanga@gmail.com>
2022-12-10 02:54:44 +0300

    storage_type

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DATETIME
-#from models import storage_type
 
 Base = declarative_base()
 
@@ -29,6 +28,8 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        from models import storage_type
+
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
