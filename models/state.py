20885c5a701e925d6ca7070ec3e430479defc824
gims-inc <gabrielmusanga@gmail.com>
2022-12-10 02:42:42 +0300

    relationship --delete

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
+                              cascade='all, delete-orphan')
     else:
         name = ''
 
