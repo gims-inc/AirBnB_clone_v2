08b413b3e5a9e2cbc7eadc3c05972d040de3533e
gims-inc <gabrielmusanga@gmail.com>
2023-01-15 23:21:08 +0300

    update --string formating

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    return f'C {text.replace("_"," ")}'
+    return 'C %s' % text.replace("_"," ")
 
 
 if __name__ == "__main__":
