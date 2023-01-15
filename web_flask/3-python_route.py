e0365b88fd3a07a1852a4efbbc26554b88016203
gims-inc <gabrielmusanga@gmail.com>
2023-01-15 23:30:33 +0300

    update --str

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,12 +20,12 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    return f'C {text.replace("_"," ")}'
+    return 'C %s' % text.replace("_", " ")
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text="is cool"):
-    return f'Python {text.replace("_"," ")}'
+    return 'Python %s' % text.replace("_", " ")}
 
 
 if __name__ == "__main__":
