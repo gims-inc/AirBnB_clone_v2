99255154f5ad776a29063b0872a6f9847b2c116c
gims-inc <gabrielmusanga@gmail.com>
2023-01-15 23:58:57 +0300

    --str formating

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -20,18 +20,18 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    return f'C {text.replace("_"," ")}'
+    return 'C %s' % text.replace("_", " ")
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
 def python_is_cool(text="is cool"):
-    return f'Python {text.replace("_"," ")}'
+    return 'Python %s' % text.replace("_", " ")
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_only(n):
-    return f'{n} is a number'
+    return '%s is a number' % n
 
 
 if __name__ == "__main__":
