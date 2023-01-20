534b5c636aba80327b16d2ccdba7825b21489cae
gims-inc <gabrielmusanga@gmail.com>
2023-01-20 20:20:22 +0300

    update

--- models/city.py
+++ models/city.py
@@ -1,19 +1,30 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
+#!/usr/bin/python
+""" holds class City"""
+import models
 from models.base_model import BaseModel, Base
-from models import storage_type
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
+    """Represents a city for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table cities.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
+    """
+    if models.storage_type == "db":
+        __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete-orphan')
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", caascade="delete")
     else:
-        name = ''
-        state_id = ''
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
